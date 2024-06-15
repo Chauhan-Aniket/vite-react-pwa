76bfb988078f10f25810f0290bdf49917a1ae520
Chauhan-Aniket <techaniac21@gmail.com>
2024-06-15 16:53:59 +0530

    feat: PWA with minimal requirements
    https-localhost for PWA testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: [
+				"favicon.ico",
+				"favicon-16x16.png",
+				"favicon-32x32.png",
+				"apple-touch-icon.png",
+				"vite.svg",
+			],
+			manifest: {
+				name: "My PWA App",
+				short_name: "PWA",
+				description: "My Awesome App description",
+				theme_color: "#FFFFFF",
+				icons: [
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-maskable-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "/pwa-maskable-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+				start_url: "/",
+				display: "standalone",
+				background_color: "#FFFFFF",
+			},
+		}),
+	],
+});
